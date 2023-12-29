3e44c4b6d8f77966998121a02ae250cc99557e52
GabrielLima2803 <gabriellima2803@gmail.com>
2023-12-29 19:59:29 -0300

    <Add> Router User

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,13 @@ const express = require('express')
 const db = require('./src/config/db')
 const cors = require('cors');
 const port = 3000
+const authRouter = require('./src/routes/authRouter')
 const app = express()
 
 app.use(cors());
 app.use(express.json())
+
+app.use('auth', authRouter);
 
 app.get('/', (req, res) => {
     res.status(200).json({ msg: 'Bem vindo a nossa API' });
