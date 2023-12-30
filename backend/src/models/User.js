16f2febcf9b0dd3ddc89a4d7e0e12ea3dad7a1fe
GabrielLima2803 <gabriellima2803@gmail.com>
2023-12-30 16:04:00 -0300

    <Add> array Task and function updatePassword

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -5,8 +5,18 @@ const userSchema = new mongoose.Schema({
     name: String,
     email: String,
     password: String,
+    Tasks: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Task'
+    }],
 })
 
-const User = mongoose.model('User', userSchema);
 
+userSchema.statics.updatePassword = async function (email, newPassword) {
+    const hashedPassword = await argon2.hash(newPassword);
+  
+    await this.findOneAndUpdate({ email }, { password: hashedPassword });
+  };
+  
+const User = mongoose.model('User', userSchema);
 module.exports = User
