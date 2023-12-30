83d56e4facc9999517a37abdccb40713eb268e92
GabrielLima2803 <gabriellima2803@gmail.com>
2023-12-30 16:01:17 -0300

    <Add> require('dotenv').config();

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,28 @@
-const express = require('express')
-const db = require('./src/config/db')
+require('dotenv').config();
+const express = require('express');
+const db = require('./src/config/db');
 const cors = require('cors');
-const port = 3000
-const authRouter = require('./src/routes/authRouter')
-const app = express()
+const port = 3000;
+const authRouter = require('./src/routes/authRouter');
+const taskRouter = require('./src/routes/taskRouter');
+const app = express();
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
-app.use('auth', authRouter);
+app.use('/auth', authRouter);
+app.use('/task', taskRouter);
 
 app.get('/', (req, res) => {
-    res.status(200).json({ msg: 'Bem vindo a nossa API' });
+    res.status(200).json({ msg: 'Bem-vindo à nossa API, API para Lista de Tarefas' });
 });
 
 db.connectMongoDb()
-.then(() => {
-    app.listen(port, () => {
-        console.log(`Servidor sendo rodado na porta ${port}`);
-      });
-}).catch((err) => {
-    console.error('Falha ao Conectar', err);
-});
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Servidor rodando na porta ${port}`);
+        });
+    })
+    .catch((err) => {
+        console.error('Falha ao conectar', err);
+    });
