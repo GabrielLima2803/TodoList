843a35af4f518005e1b5592da136f97fe93388f2
GabrielLima2803 <gabriellima2803@gmail.com>
2023-12-30 16:02:24 -0300

    <Add>console.log

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,11 +3,12 @@ const mongoose = require('mongoose');
 async function connectMongoDb() {
     try {
         await mongoose.connect('mongodb://127.0.0.1:27017/TodoList');
+        console.log(`Conectado ao banco`)
     } catch (error) {
         console.error('Falha ao conectar ao banco de dados', error)
     }
 }
 
-module.export = {
+module.exports = {
     connectMongoDb,
 }
