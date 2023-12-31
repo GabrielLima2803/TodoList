ae44a31c0a326af5811cb34bb1a0fe65039f9b91
GabrielLima2803 <gabriellima2803@gmail.com>
2023-12-30 22:38:35 -0300

    <Add> routes in task

--- backend/src/routes/taskRouter.js
+++ backend/src/routes/taskRouter.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const taskController = require('../controllers/taskController');
+const checkToken = require('../middleware/checkToken');
 
 const router = express.Router();
 
-router.post('/add-task', taskController.AddTask);
+router.post('/add-task', checkToken, taskController.addTask);
+router.get('/get-task', checkToken, taskController.getTask);
+router.put('/put-task/:_id', checkToken, taskController.updateTask);
 
 
 
