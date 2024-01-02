96e277f714f976a3bc9e436a1bd1fd44d9b962f6
GabrielLima2803 <gabriellima2803@gmail.com>
2024-01-02 20:00:30 -0300

    <Add> New Paste

---
+++ frontend/src/components/footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+    return(
+        <div className="footer">
+        <h5  className="text-center">Feito por Gabriel Lima de Souza</h5>
+        <h6  className="text-center">Código Github</h6>
+    </div>
+    )
+}
+
+export default Footer
