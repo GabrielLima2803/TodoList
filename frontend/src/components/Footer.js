5e768f01182178c517cc2f9c824e29b280803b84
GabrielLima2803 <gabriellima2803@gmail.com>
2024-01-01 22:38:57 -0300

    <Fix> return

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,9 +1,12 @@
 import React from "react";
 
 const Footer = () => {
-    <div class = 'footer'>
-        <h2>Feito por Gabriel Lima de Souza</h2>
+    return(
+        <div class = 'footer'>
+        <h5  className="text-center">Feito por Gabriel Lima de Souza</h5>
+        <h6  className="text-center">Código Github</h6>
     </div>
+    )
 }
 
 export default Footer
