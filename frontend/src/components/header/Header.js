cb8bdd306fc668817f088e4fb1de158d75436aa6
GabrielLima2803 <gabriellima2803@gmail.com>
2024-01-02 20:00:47 -0300

    <Add> New Paste Header

---
+++ frontend/src/components/header/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div id="headerTop">
+      <div class="house">
+        <div to="/">
+          <i class="bi bi-house-door color-house"></i>
+        </div>
+      </div>
+      <div>
+
+      </div>
+      <div class="linksTop">
+        <div to="/login" class="text-black text">
+          <span class="borde-Ultimo"><i class="bi bi-person"></i></span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
