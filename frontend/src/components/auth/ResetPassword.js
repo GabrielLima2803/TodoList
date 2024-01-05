3e8b4d9e676a0f0a082085ec4e5c4f6b5c78541e
GabrielLima2803 <gabriellima2803@gmail.com>
2024-01-04 23:28:05 -0300

    <Add> method http reset-password

--- frontend/src/components/auth/ResetPassword.js
+++ frontend/src/components/auth/ResetPassword.js
@@ -1,56 +1,108 @@
-import React from "react";
+import React, { useState } from "react";
 import "./auth.css";
 import { Link } from "react-router-dom";
 
 const ResetPassword = () => {
-  return(
-    <div className="login-container">
-    <div className="login-form">
-      <form className="container-def">
-        <h1 className="text-center mb-5 mt-2">Reset de senha</h1>
-        <div className="container-user container-def form-floating">
-          <input
-            type="email"
-            className="form-control"
-            id="floatingInput"
-            placeholder="email"
-          />
-          <label for="floatingInput">Email</label>
-        </div>
-        <div className="container-pass container-def form-floating">
-          <input
-            type="text"
-            className="form-control"
-            id="floatingInput"
-            placeholder="code"
-          />
-          <label for="floatingInput">
-            Code
-          </label>
-        </div>
-        <div className="container-pass container-def form-floating">
-          <input
-            type="password"
-            className="form-control"
-            id="floatingInput"
-            placeholder="Nova senha"
-          />
-          <label for="floatingInput">
-            Nova Senha
-          </label>
-        </div>
-        <button type="submit" className="button-entrar">
-          Resetar
-        </button>
-        <div className="links">
-          <Link className="register-link" to="/register">
-            <div>Não tem conta? Register</div>
-          </Link>
-        </div>
-      </form>
-    </div>
-  </div>
-  )
+  const [formData, setFormData] = useState({
+    email: "",
+    code: "",
+    newPassword: "",
+  });
+
+  const [successMessage, setSuccessMessage] = useState(null);
+  const [error, setError] = useState(null);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
   };
-  
-  export default ResetPassword;
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(
+        "http://localhost:3000/auth/reset-password",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+
+      const data = await response.json();
+
+      if (response.ok) {
+        setSuccessMessage(data.msg);
+        setError(null);
+      } else {
+        setSuccessMessage(null);
+        setError(data.error || "Erro interno do servidor");
+      }
+    } catch (error) {
+      setSuccessMessage(null);
+      setError("Erro ao fazer a requisição. Tente novamente mais tarde.");
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <div className="login-form">
+        <form className="container-def" onSubmit={handleSubmit}>
+          <h1 className="text-center mb-5 mt-2">Reset de senha</h1>
+
+          <div className="container-user container-def form-floating">
+            <input
+              type="email"
+              className="form-control"
+              id="emailInput"
+              name="email"
+              placeholder="Email"
+              onChange={handleChange}
+            />
+            <label htmlFor="emailInput">Email</label>
+          </div>
+          <div className="container-pass container-def form-floating">
+            <input
+              type="text"
+              className="form-control"
+              id="codeInput"
+              name="code"
+              placeholder="Code"
+              onChange={handleChange}
+            />
+            <label htmlFor="codeInput">Code</label>
+          </div>
+          <div className="container-pass container-def form-floating">
+            <input
+              type="password"
+              className="form-control"
+              id="newPasswordInput"
+              name="newPassword"
+              placeholder="Nova Senha"
+              onChange={handleChange}
+            />
+            <label htmlFor="newPasswordInput">Nova Senha</label>
+          </div>
+          <button type="submit" className="button-entrar">
+            Resetar
+          </button>
+          <div className="links">
+            {error && <p className="error-message mt-2">{error}</p>}
+            {successMessage && (
+              <div>
+                <p className="success-message mt-2">{successMessage}</p>
+                <Link className="register-link" to="/login">
+                  <div>Login</div>
+                </Link>
+              </div>
+            )}
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default ResetPassword;
