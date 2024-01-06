981e02bf9014db1d2a5a64686ef4ebb02e8dfded
GabrielLima2803 <gabriellima2803@gmail.com>
2024-01-05 21:23:25 -0300

    <Fix> createTask model

--- frontend/src/components/modals/createTask.js
+++ frontend/src/components/modals/createTask.js
@@ -1,53 +1,62 @@
 import React, { useState } from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
-const CreateTaskPopup = ({modal, toggle, save}) => {
-    const [taskName, setTaskName] = useState('');
-    const [description, setDescription] = useState('');
+const CreateTaskPopup = ({ modal, toggle, save }) => {
+  const [taskName, setTaskName] = useState('');
+  const [description, setDescription] = useState('');
 
-    const handleChange = (e) => {
-        
-        const {name, value} = e.target
-
-        if(name === "taskName"){
-            setTaskName(value)
-        }else{
-            setDescription(value)
-        }
-
-
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    if (name === 'taskName') {
+      setTaskName(value);
+    } else {
+      setDescription(value);
     }
+  };
 
-    const handleSave = (e) => {
-        e.preventDefault()
-        let taskObj = {}
-        taskObj["Name"] = taskName
-        taskObj["Description"] = description
-        save(taskObj)
+  const handleSave = (e) => {
+    e.preventDefault();
+    let taskObj = {};
+    taskObj['name'] = taskName;
+    taskObj['description'] = description;
+    save(taskObj);
+  };
 
-    }
-
-    return (
-        <Modal isOpen={modal} toggle={toggle}>
-            <ModalHeader toggle={toggle}>Criar Tarefa</ModalHeader>
-            <ModalBody>
-            
-                    <div className = "form-group">
-                        <label>Nome da tarefa</label>
-                        <input type="text" className = "form-control" value = {taskName} onChange = {handleChange} name = "taskName"/>
-                    </div>
-                    <div className = "form-group">
-                        <label>Descrição</label>
-                        <textarea rows = "5" className = "form-control" value = {description} onChange = {handleChange} name = "description"></textarea>
-                    </div>
-                
-            </ModalBody>
-            <ModalFooter>
-            <Button color="primary" onClick={handleSave}>Criar</Button>{' '}
-            <Button color="secondary" onClick={toggle}>Cancelar</Button>
-            </ModalFooter>
-      </Modal>
-    );
+  return (
+    <Modal isOpen={modal} toggle={toggle}>
+      <ModalHeader toggle={toggle}>Criar Tarefa</ModalHeader>
+      <ModalBody>
+        <div className="form-group">
+          <label>Nome da tarefa</label>
+          <input
+            type="text"
+            className="form-control"
+            value={taskName}
+            onChange={handleChange}
+            name="taskName"
+          />
+        </div>
+        <div className="form-group">
+          <label>Descrição</label>
+          <textarea
+            rows="5"
+            className="form-control"
+            value={description}
+            onChange={handleChange}
+            name="description"
+          ></textarea>
+        </div>
+      </ModalBody>
+      <ModalFooter>
+        <Button color="primary" onClick={handleSave}>
+          Criar
+        </Button>{' '}
+        <Button color="secondary" onClick={toggle}>
+          Cancelar
+        </Button>
+      </ModalFooter>
+    </Modal>
+  );
 };
 
 export default CreateTaskPopup;
