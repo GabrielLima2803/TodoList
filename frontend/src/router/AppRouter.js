04888005c0a444038b4e5f74cda6ea3a23911823
GabrielLima2803 <gabriellima2803@gmail.com>
2024-01-06 21:59:17 -0300

    <Add> Router home and Fix Todo-list router

--- frontend/src/router/AppRouter.js
+++ frontend/src/router/AppRouter.js
@@ -1,20 +1,22 @@
 // AppRouter.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from '../components/auth/Login';
-import Register from '../components/auth/Register';
-import ForgotPassword from '../components/auth/ForgotPassword';
-import ResetPassword from '../components/auth/ResetPassword';
+import Login from '../pages/auth/Login';
+import Register from '../pages/auth/Register';
+import ForgotPassword from '../pages/auth/ForgotPassword';
+import ResetPassword from '../pages/auth/ResetPassword';
+import Home from '../pages/Home'
 import TodoList from '../components/TodoList';
 
 const AppRouter = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<TodoList />} />
+        <Route path="/todo-list" element={<TodoList />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register/>} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/" element={<Home/>}/>
         <Route path="/reset-password" element={<ResetPassword />} />
       </Routes>
     </Router>
