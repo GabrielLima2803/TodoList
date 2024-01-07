aa386d1fa96d22d1c0139c3235df4646701d16d0
GabrielLima2803 <gabriellima2803@gmail.com>
2024-01-06 21:55:56 -0300

    <Add> Name User in header

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-
 const Header = () => {
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  const [userName, setUserName] = useState("");
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem("authToken");
+    setLoggedIn(!!storedToken);
+
+    if (isLoggedIn) {
+      const storedUserInfo = localStorage.getItem("userInfo");
+      const user = storedUserInfo ? JSON.parse(storedUserInfo) : { name: "" };
+      setUserName(user.name);
+    }
+  }, [isLoggedIn]);
+
   return (
     <div id="headerTop">
       <div className="house">
@@ -11,6 +24,7 @@ const Header = () => {
         </Link>
       </div>
       <div>
+        {isLoggedIn && <p className="AuthName">{`Lista de tarefas de ${userName}`}</p>}
       </div>
       <div className="linksTop">
         <Link to="/login" className="text-black text">
